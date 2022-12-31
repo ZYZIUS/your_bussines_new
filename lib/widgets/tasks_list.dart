fbe90cbbf232d2d9f73a6e76c336460aca73a7fe
ZYZU <dev@ilashop.pl>
2022-12-31 15:30:01 +0100

    HotFix

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -29,22 +29,5 @@ class TasksList extends StatelessWidget {
         );
       },
     );
-
-    // return ListView(
-    //   children: <Widget>[
-    //     TaskTile(
-    //       taskTitle: tasks[0].name,
-    //       isChecked: tasks[0].isDone,
-    //     ),
-    //     TaskTile(
-    //       taskTitle: tasks[1].name,
-    //       isChecked: tasks[1].isDone,
-    //     ),
-    //     TaskTile(
-    //       taskTitle: tasks[2].name,
-    //       isChecked: tasks[2].isDone,
-    //     ),
-    //   ],
-    // );
   }
 }
