badb79fca3d1c6ce0429b197d5c7fb31863f9417
ZYZU <dev@ilashop.pl>
2023-01-01 14:44:15 +0100

    Dodanie nowej podstrony

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -12,7 +12,6 @@ class TasksScreen extends StatefulWidget {
 }
 
 class _TasksScreenState extends State<TasksScreen> {
-  final int _selectedIndex = 0;
   void _onItemTapped(int index, BuildContext context) {
     if (index == 1) {
       showModalBottomSheet(
@@ -25,6 +24,11 @@ class _TasksScreenState extends State<TasksScreen> {
             child: AddTaskScreen(),
           ),
         ),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Profil()),
       );
     }
   }
@@ -111,6 +115,102 @@ class _TasksScreenState extends State<TasksScreen> {
                 ),
               ),
               child: const TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Profil extends StatelessWidget {
+  const Profil({Key? key}) : super(key: key);
+  void _onItemTapped(int index, BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TasksScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            // sets the background color of the `BottomNavigationBar`
+            canvasColor: const Color.fromARGB(255, 30, 30, 30),
+            // sets the active color of the `BottomNavigationBar` if `Brightness` is light
+            primaryColor: Colors.white,
+            textTheme: Theme.of(context).textTheme.copyWith(
+                caption: const TextStyle(
+                    color: Colors
+                        .yellow))), // sets the inactive color of the `BottomNavigationBar`
+        child: BottomNavigationBar(
+          fixedColor: Colors.white,
+          unselectedItemColor: Colors.white, //<-- add this
+          type: BottomNavigationBarType.fixed,
+          currentIndex: 0,
+          onTap: (int index) {
+            _onItemTapped(index, context);
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.task),
+              label: 'Zadania',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.keyboard_return,
+                  color: Color.fromARGB(255, 255, 255, 255)),
+              label: 'Cofnij',
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 30, 30, 30),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 40.0, left: 30.0, right: 20.0, bottom: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  'Profil',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                Text(
+                  'Edytuj lub zmień',
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 45, 45, 45),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Container(),
             ),
           ),
         ],
